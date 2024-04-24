03f2a445f24ee6cae6d8e40309d165ca8eb7180e
abdulazeez <bello.m1605299@st.futminna.edu.ng>
2024-04-24 08:52:48 +0100

    update context

--- Shop.persistence/Contexts/AppDbContext.cs
+++ Shop.persistence/Contexts/AppDbContext.cs
@@ -1,11 +1,6 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 using Shop.Domain.Common.Entities;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 
 namespace Shop.persistence.Contexts
 {
